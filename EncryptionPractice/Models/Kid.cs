d7aeb25b925d82684cdaa26f184b80cd9948b600
MegMoore <136514941+MegMoore@users.noreply.github.com>
2023-12-14 13:20:07 -0500

    Add project files.

---
+++ EncryptionPractice/Models/Kid.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EncryptionPractice.Models
+{
+    public class Kid
+    {
+        public int Id { get; set; }
+        [StringLength(30)]
+        public string FirstName { get; set; } = string.Empty;
+        [StringLength(30)]
+        public string LastName { get; set; } = string.Empty;
+        [StringLength(80)]
+        public string Email { get; set; } = string.Empty;
+        [StringLength(80)]
+        public string HashPassword { get; set; } = string.Empty;
+        [StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+        [StringLength(12)]
+        public string Phone { get; set; } = string.Empty;
+    }  
+}
