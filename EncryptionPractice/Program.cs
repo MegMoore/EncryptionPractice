085316b7536224d3e0b86dc0a5fc8519ddc27af7
MegMoore <136514941+MegMoore@users.noreply.github.com>
2023-12-15 12:38:52 -0500

    Initial commit

--- EncryptionPractice/Program.cs
+++ EncryptionPractice/Program.cs
@@ -3,13 +3,16 @@ using Microsoft.Extensions.DependencyInjection;
 using EncryptionPractice.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<EncryptionPracticeContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("EncryptionPracticeContext") ?? throw new InvalidOperationException("Connection string 'EncryptionPracticeContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevDb") ?? throw new InvalidOperationException("Connection string 'EncryptionPracticeContext' not found.")));
+
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
